3fb36d0f2466c72aa4dee365ce816708dbd36246
Ilya Sh <shefchenkornd@gmail.com>
2021-10-22 20:37:26 +0300

    Функции как параметры других функций

--- func/func_type/func_type.go
+++ func/func_type/func_type.go
@@ -1,9 +1,20 @@
 package main
 
-func add()  {
-	
+import "fmt"
+
+func add(a, b int) int {
+	return a + b
+}
+
+func sub(x, y int) int {
+	return x - y
+}
+
+func action(a, b int, operation func(int, int) int) {
+	result := operation(a, b)
+	fmt.Println(result)
 }
 
 func main() {
-
+	action( 3, 4, add)
 }
