f03e398d1e93540e30631c8a3a7fd771de6e3fc8
Ilya Sh <shefchenkornd@gmail.com>
2021-10-31 21:09:55 +0300

    Бенчмарки конкатенации строк

--- concatString/main_test.go
+++ concatString/main_test.go
@@ -2,5 +2,43 @@ package main
 
 import "testing"
 
-func Test_cBytes(t *testing.T) {
+// 65 ns/op
+func Benchmark_cBytes(b *testing.B) {
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cBytes(params)
+	}
+
+	b.StopTimer()
+}
+
+// 99 ns/op
+func Benchmark_cPlus(b *testing.B) {
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cPlus(params)
+	}
+
+	b.StopTimer()
+}
+
+// 78 ns/op
+func Benchmark_cStrings(b *testing.B) {
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cStrings(params)
+	}
+
+	b.StopTimer()
+}
+
+
+// 50 ns/op
+func Benchmark_cCopy(b *testing.B) {
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		cCopy(params)
+	}
+
+	b.StopTimer()
 }
