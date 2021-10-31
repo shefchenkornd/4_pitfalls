3ec95290f395d7ea74de060efe17bd79526e2121
Ilya Sh <shefchenkornd@gmail.com>
2021-10-31 20:46:52 +0300

    Конкатенация строк - 4 способа

--- concatString/main.go
+++ concatString/main.go
@@ -1,5 +1,57 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"fmt"
+	"strings"
+)
 
+var params = []string{"string-1", "string-2", "string-3", "string-4"}
+
+func main() {
+	fmt.Println(cPlus(params))
+	fmt.Println(cBytes(params))
+	fmt.Println(cStrings(params))
+	fmt.Println(cCopy(params))
+}
+
+func cCopy(args []string) string {
+	lenB := make([]byte, len(args)*len(args[0]))
+	offset := 0
+	for _, str := range args {
+		fmt.Println(offset)
+		offset += copy(lenB[offset:], str)
+	}
+
+	return string(lenB[:])
+}
+
+func cPlus(args []string) string {
+	var result string
+
+	for _, str := range args {
+		result += str
+	}
+
+	return result
+}
+
+func cBytes(args []string) string {
+	buffer := bytes.Buffer{}
+
+	for _, str := range args {
+		buffer.WriteString(str)
+	}
+
+	return buffer.String()
+}
+
+func cStrings(args []string) string {
+	builder := strings.Builder{}
+
+	for _, str := range args {
+		builder.WriteString(str)
+	}
+
+	return builder.String()
 }
