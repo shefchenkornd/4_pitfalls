b433cfc50b990a157ddd5bac49f37107a2e49ded
Ilya Sh <shefchenkornd@gmail.com>
2021-10-31 20:19:50 +0300

    Interface

---
+++ interface/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type Vehicle interface {
+	move()
+}
+
+type Car struct{}
+type Airplane struct{}
+
+func (c Car) move() {
+	fmt.Println("Автомобиль едет")
+}
+
+func (a Airplane) move() {
+	fmt.Println("Самолёт летит")
+}
+
+func drive(v Vehicle)  {
+	v.move()
+}
+
+func main() {
+	vesta := Car{}
+	vesta.move()
+
+	boing := Airplane{}
+	boing.move()
+
+	// OR
+	var tesla Vehicle = Car{}
+	tesla.move()
+
+	var tu104 Vehicle = Airplane{}
+	tu104.move()
+
+	// OR
+	drive(vesta)
+	drive(boing)
+}
