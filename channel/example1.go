6d6a2478bfaf0feb1c236ce5dff7df7ed39f9910
Ilya Sh <shefchenkornd@gmail.com>
2021-11-07 14:04:51 +0300

    channels

---
+++ channel/example1.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	intCh := make(chan int)
+
+	for i := 0; i <= 5; i++ {
+		go factorial(i, intCh)
+
+		fmt.Println(<-intCh)
+	}
+}
+
+func factorial(n int, ch chan int) {
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
+	}
+
+	ch <- result // отправка данных в канал
+}
--- channel/main.go
+++ channel/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	fmt.Println("main function starts")
+	intCh := make(chan int)
+
+	go func() {
+		fmt.Println("Go routine starts")
+		intCh<-5
+	}()
+
+	fmt.Println("Get value from channel", <-intCh) // получение данных из канала
+	fmt.Println("The End")
 }
