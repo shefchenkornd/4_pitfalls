897b3588c0bf2fc1f3be2c7eba3f8e58d0eff9b9
Ilya Sh <shefchenkornd@gmail.com>
2021-11-07 14:09:40 +0300

    Буфферизированный канал

--- channel/buf_channel.go
+++ channel/buf_channel.go
@@ -1,5 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+	intCh := make(chan int, 3)
+
+	intCh<-1
+	intCh<-2
+	intCh<-3
+
+	fmt.Println(<-intCh)     // 1
+	fmt.Println(<-intCh)     // 2
+	fmt.Println(<-intCh)     // 3
 }
