4cb4620b19a61af5498851f60d1f65e1fb358e30
Ilya Sh <shefchenkornd@gmail.com>
2021-11-20 17:36:02 +0300

    io.Writer interface (additional)

--- reader_writer/writer/main.go
+++ reader_writer/writer/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 // Метод Write предназначен для копирования данных их среза байт p в определенный ресурс - файл, сетевой интерфейс и т.д.
 // Метод возвращает количество записанных байтов и объект ошибки.
 
-type phoneWriter struct{}
+type phoneWriter struct{
+	phone string
+}
 
 func (ph phoneWriter) Write(bs []byte) (int, error) {
 	countNumber := 0
+	var sb strings.Builder
+
 	for _, r := range bs {
 		if r >= 48 && r <= 59 {
+			sb.WriteString(string(r))
+
 			fmt.Print(string(r))
 			countNumber++
 		}
 	}
+
+	ph.phone = sb.String()
 
 	return countNumber, nil
 }
@@ -26,4 +35,6 @@ func main() {
 
 	ph := phoneWriter{}
 	ph.Write(bytes)
+
+	fmt.Println(ph.phone)
 }
