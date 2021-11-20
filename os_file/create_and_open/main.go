7f53f9b3737b37576a80573b79da3e69aa2870cb
Ilya Sh <shefchenkornd@gmail.com>
2021-11-20 18:11:53 +0300

    os.File create and read

--- os_file/create_and_open/main.go
+++ os_file/create_and_open/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	$END$
+	file, err := os.Create("file.go")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	fmt.Println(file.Name())
+
+	b := make([]byte, 100)
+	var count int
+	count, err = file.Read(b)
+	if err != nil {
+		if io.EOF != err {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Printf(
+		"read %d bytes: %q\n",
+		count,
+		b[:count],
+	)
 }
