27d9898d19408a04966bd0c317b8bd7d2f823660
Ilya Sh <shefchenkornd@gmail.com>
2021-11-21 22:11:35 +0300

    io.Copy

--- os_file/io_copy/main.go
+++ os_file/io_copy/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
 func main() {
-	$END$
+	file, err := os.Open("hello.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	wr, err := io.Copy(os.Stdout, file)
+	if err != nil {
+		log.Fatal("io.Copy", err)
+	}
+	fmt.Println(wr, err)
 }
