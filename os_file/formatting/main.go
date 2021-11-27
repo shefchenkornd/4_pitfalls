a48e01b4b19929b56efb1da7864c615fd327152f
Ilya Sh <shefchenkornd@gmail.com>
2021-11-27 13:30:49 +0300

    Formatting

---
+++ os_file/formatting/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	file, err := os.Create("confeve.txt")
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+	fmt.Fprint(file, "Сегодня ")
+	fmt.Fprintln(file, "хорошая погода")
+}
