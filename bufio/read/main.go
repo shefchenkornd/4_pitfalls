f0218f5c06977fc6848c6e15bb4e2d794a48c6df
Ilya Sh <shefchenkornd@gmail.com>
2021-11-27 15:10:25 +0300

    Чтение через буфер bufio

--- bufio/read/main.go
+++ bufio/read/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
+// Буферизированный ввод-вывод
+// Чтение через буфер
+func main() {
+	file, err := os.Open("some.dat")
+	if err != nil {
+		log.Fatalln("Unable to open file:", err)
+	}
+	defer file.Close()
+
+	b := bufio.NewReader(file)
+	for {
+		str, err := b.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				fmt.Println(err)
+			}
+		}
+
+		fmt.Print(str)
+	}
 }
