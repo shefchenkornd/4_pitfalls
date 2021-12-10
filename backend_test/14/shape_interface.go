c70a7e0b8937c33fc3aaa36e8b672e4f38bd94ec
Ilya Sh <shefchenkornd@gmail.com>
2021-12-10 19:49:45 +0300

    Shape interface & struct

---
+++ backend_test/14/shape_interface.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Shape interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Rect struct {
+	width float64
+	height float64
+}
+
+func (r Rect) Area() float64 {
+	return r.width * r.height
+}
+
+func (r Rect) Perimeter() float64 {
+	return 2 * (r.width + r.height)
+}
+
+// Consider the following code:
+// What is the output?
+func main() {
+	r := Rect{5.0, 4.0}
+	var s Shape = r
+
+	area := s.Area()
+	fmt.Println(area) // 20
+}
