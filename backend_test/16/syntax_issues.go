2f6d09fc03ab2272b8ce0aaa34972d2b4f064312
Ilya Sh <shefchenkornd@gmail.com>
2021-12-11 11:51:49 +0300

    syntax issues in code

---
+++ backend_test/16/syntax_issues.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type S struct{}
+
+func f(x interface{}) {}
+
+func g(x *interface{}) {
+
+	fmt.Printf("%T",  x)
+}
+
+// Consider the following lines of code, marked with an A, B, C and D.
+// Which of these have syntax issues (choose one or more)?
+func main() {
+	s := S{}
+	p := &s
+
+	f(s) // A
+	g(s) // B
+	f(p) // C
+	g(p) // D
+
+}
+// Answer: B and D
