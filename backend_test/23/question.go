bf320b72c00529ee3653ee5d27fdab7961412ce2
Ilya Sh <shefchenkornd@gmail.com>
2021-12-11 14:21:10 +0300

    About goroutines vs threads

---
+++ backend_test/23/question.go
@@ -0,0 +1,17 @@
+package question
+
+// Question:
+// Which of following are true about goroutines vs threads (choose one or more)?
+
+
+/*
+a: Goroutines have growable segmented stacks.
+b: Goroutines map directly to OS threads, and are relatively heavyweight.
+c: Goroutines are multiplexed onto a small number of OS threads, rather than a 1:1 mapping
+d: Goroutines communicate via callbacks.
+
+ */
+
+
+
+// Answer: a, c
