05563e18dc11335d8e9e1891b4c49632b982dbf2
Ilya Sh <shefchenkornd@gmail.com>
2022-02-27 12:51:56 +0300

    :sparkles: compare two slices of byte

--- 30_compare_struct_array_slice_map/example1.go
+++ 30_compare_struct_array_slice_map/example1.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 type data struct {
 	num     int
@@ -21,4 +24,22 @@ func main() {
 	v1 := data{}
 	v2 := data{}
 	fmt.Println("v1 == v2:",v1 == v2) // выводит: v1 == v2: true
+
+
+	// Compare slice of byte.
+
+	sliceByte1 := []byte{'G', 'E', 'E', 'K', 'S'}
+	sliceByte2 := []byte{'G', 'E', 'e', 'K', 'S'}
+	// fmt.Println(slice_byte_1 == slice_byte_2) 	// Сравнивать slice of byte напрямую нельзя!
+
+	// Using Compare function
+	res := bytes.Compare(sliceByte1, sliceByte2)
+
+	if res == 0 {
+		fmt.Println("!..Slices are equal..!")
+	} else {
+		fmt.Println("!..Slice are not equal..!") // this is output
+	}
+
+
 }
